72976f5f07f01ac7330fdbf7e0892b124208c9d2
cruz4 <cruz@raccoon.ag>
2020-10-16 16:31:56 -0300

    get products from subcategory page finished

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ from selenium import webdriver
 import json
 
 
-
+def get_products_from_subcategory_page(url):
+    offset = 0
+    
+    while True:
+        driver = webdriver.Firefox()
+        driver.get(url+"?offset="+str(offset))
+        offset=offset+36
+        elements = driver.find_elements_by_css_selector('a[data-clicktype="product_tile_click"][href]')
+        product_links = [elem.get_attribute('href') for elem in elements]
+        product_links = list( dict.fromkeys(product_links) )
+        print(len(product_links))
+        datalayer = driver.execute_script('return dataLayer')
+        driver.quit()
+        if(len(datalayer["products"])!=36):
+            break
 
 
 
@@ -15,5 +29,7 @@ def get_object_properties_single_page(url):
 
 
 if __name__ == "__main__":
-    product_info = get_object_properties_single_page("https://www.lowes.com/pd/Whirlpool-24-5-cu-ft-4-Door-French-Door-Refrigerator-with-Ice-Maker-Fingerprint-Resistant-Stainless-Steel-ENERGY-STAR/1000257811")
-    print(product_info)
+    #product_info = get_object_properties_single_page("https://www.lowes.com/pd/Whirlpool-24-5-cu-ft-4-Door-French-Door-Refrigerator-with-Ice-Maker-Fingerprint-Resistant-Stainless-Steel-ENERGY-STAR/1000257811")
+    #print(product_info)
+    element = get_products_from_subcategory_page("https://www.lowes.com/pl/Side-by-side-refrigerators-Refrigerators-Appliances/4294857970")
+    
